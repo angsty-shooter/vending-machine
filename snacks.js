c27e3226cdf6080e044d84030af1019340d2ba7e
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-16 15:23:34 -0700

    all the files

---
+++ snacks.js
@@ -0,0 +1,12 @@
+
+class Snacks{
+    constructor(
+        price = 0
+){
+    this.price = price;
+}}
+
+let soda = new Snacks (2.00);
+let skittles = new Snacks (0.50)
+let chips = new Snacks (0.50)
+let beefJerky = new Snacks (3.00)
